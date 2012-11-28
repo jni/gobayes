2827ca7942d39990cc969f8691109da55cd79b22
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2012-11-28 16:12:12 +1100

    Refactor make_annotations_dict

--- gobayes/tools.py
+++ gobayes/tools.py
@@ -1,4 +1,4 @@
-
+from collections import defaultdict
 import networkx as nx
 
 class IdentityDict(object):
@@ -7,22 +7,24 @@ class IdentityDict(object):
     def __getitem__(self, x):
         return x
 
-def make_annotation_dict(annot_table, ontology, canon=IdentityDict(),
-                         trace=True, gene_id_column=2, annot_column=4):
-    raw_annotations = [
-        (annot_row[gene_id_column], canon[annot_row[annot_column]])
-        for annot_row in annot_table]
-    if trace:
+def make_annotation_dict(annot_table, ontology=None, canon=IdentityDict(),
+                         gene_id_column=2, annot_column=4):
+    annot_dict = {}
+    if ontology is not None:
         trace = trace_ontology(ontology)
-        for gene, annot in raw_annotations:
-            inferred = trace[annot]
-            for inf_annot in inferred:
-                raw_annotations.append((gene, inf_annot))
-    return raw_annotations
+    else:
+        trace = defaultdict(list)
+    for annot_row in annot_table:
+        gene = annot_row[gene_id_column]
+        annot = canon[annot_row[annot_column]]
+        annot_dict.setdefault(gene, set([])).add(annot)
+        for annot in trace[annot]:
+            annot_dict[gene].add(annot)
+    return annot_dict
 
 def trace_ontology(ontology):
     descendants = nx.algorithms.dag.descendants
     d = {}
     for node in ontology:
-        d[node] = descendants(ontology, node, False)
+        d[node] = descendants(ontology, node)
     return d
