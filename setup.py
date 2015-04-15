283842182e76012ff8c739636524a3793a730865
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2015-04-15 21:30:59 +1000

    Add setuptools support

---
+++ setup.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import
+#from distutils.core import setup
+from setuptools import setup
+
+descr = """GO-Bayes: A simple Bayesian correction for GO overrepresentation.
+"""
+
+DISTNAME            = 'gobayes'
+DESCRIPTION         = 'Bayesian correction for overrepresentation tests'
+LONG_DESCRIPTION    = descr
+MAINTAINER          = 'Juan Nunez-Iglesias'
+MAINTAINER_EMAIL    = 'juan.n@unimelb.edu.au'
+URL                 = 'https://github.com/jni/gobayes'
+LICENSE             = 'BSD 3-clause'
+DOWNLOAD_URL        = 'https://github.com/jni/gobayes'
+VERSION             = '0.1-dev'
+PYTHON_VERSION      = (3, 4)
+INST_DEPENDENCIES   = {}
+
+
+if __name__ == '__main__':
+
+    setup(name=DISTNAME,
+        version=VERSION,
+        url=URL,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        author=MAINTAINER,
+        author_email=MAINTAINER_EMAIL,
+        license=LICENSE,
+        packages=['viridis'],
+        package_data={},
+        install_requires=INST_DEPENDENCIES,
+        scripts=[]
+    )
+
